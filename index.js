91212d3733e6ec2489e68c7a277920bf02f622e5
rimasultana <rimasultana20225@gmail.com>
2024-03-18 00:04:53 +0600

    static

---
+++ index.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(name) {
+      this.name = name;
+    }
+    static hello() {
+      return "Hello!!";
+    }
+  }
+  
+  const myCar = new Car("Ford");
+  console.log(Car.hello());
